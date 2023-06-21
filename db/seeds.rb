c2fc1df0969740309d44b0f4765c2d821f80ea44
Andreia Cifoni <andreiacifoni@gmail.com>
2023-06-21 17:36:23 -0400

    starting seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+user = User.create([{username: "Mike", password: "123"},{username: "Lucas", password: "456"}])
+
+post = Post.create([{title: "First post" , url: "abc", user: user.first}, {title: "Second post" , url: "abc", user: user.second}]) 
+
+comment = Comments.create
