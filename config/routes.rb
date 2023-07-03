8154655307f272b65faaa59264aa6319ed98902c
Andreia Cifoni <andreiacifoni@gmail.com>
2023-07-03 16:28:46 -0400

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,20 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "posts#index"
+
+  resources :users
+
+  resources :posts do
+    resources :comments
+  end
+
+  post '/comments/:id/plus_point', to: 'comment_points#create'
+  post '/comments/:id/minus_point', to: 'comment_points#create'
+  delete '/comments/:id/point', to: 'comment_points#destroy'
+
+  post '/posts/:id/plus_point', to: 'post_points#create'
+  post '/posts/:id/minus_point', to: 'post_points#create'
+  delete '/posts/:id/point', to: 'post_points#destroy'
+
 end
