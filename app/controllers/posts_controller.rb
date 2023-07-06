8b5c1150b13e64611884a8e6ba3cac94f1169229
Andreia Cifoni <andreiacifoni@gmail.com>
2023-07-06 17:31:13 -0400

    started post view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,12 @@ class PostsController < ApplicationController
   end
 
   def show #display specific post
+    @post = Post.find(params[:id])
+    @post_upvote = PostPoint.where(post_id: @post.id, value: true).length
+    @post_downvote = PostPoint.where(post_id: @post.id, value: false).length
+    @post_points = @post_upvote - @post_downvote
+    #@comments = Comment.where(post_id: @post.id)
+    @comments = @post.comments
   end
 
   def update #update specific
