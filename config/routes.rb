7ad22c528df84f7f2876918ca89d16386477b347
Andreia Cifoni <andreiacifoni@gmail.com>
2023-08-01 15:20:30 -0400

    extra routes deleted, changes in reg/login form

--- config/routes.rb
+++ config/routes.rb
@@ -21,9 +21,7 @@ Rails.application.routes.draw do
   delete '/posts/:id/point', to: 'post_points#destroy'
 
   #routes for authentication
-  #get 'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
-  #get 'sign_in', to: 'sessions#new'
   post 'sign_in', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
  
