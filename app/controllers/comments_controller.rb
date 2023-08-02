de9630e0c877d4f0a5a33204738c45c8b8c850b5
Andreia Cifoni <andreiacifoni@gmail.com>
2023-08-02 16:55:41 -0400

    implemented comment form and create comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,23 @@
 class CommentsController < ApplicationController
    def create 
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    @comment.user = current_user
+
+    if @comment.save
+      redirect_to post_path(@post), notice: 'Comment was successfully created.'
+    else
+      render "posts/show"
+    end
   end
 
   def update 
   end
 
   def destroy 
+  end
+
+  def comment_params
+    params.require(:comment).permit(:comment)
   end
 end
